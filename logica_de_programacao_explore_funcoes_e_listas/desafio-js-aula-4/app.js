c90fef0c9c951496afc93e47d460da289aba5c67
Rafael Custodio <rafaelcustodio@msn.com>
2025-03-15 08:41:14 -0300

    finalizando aula 04

---
+++ logica_de_programacao_explore_funcoes_e_listas/desafio-js-aula-4/app.js
@@ -0,0 +1,35 @@
+// 1 - Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+
+
+// 2 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Python"];
+console.log(linguagensDeProgramacao);
+
+
+// 3 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java");
+console.log(linguagensDeProgramacao);
+linguagensDeProgramacao.push("Ruby", "GoLang");
+console.log(linguagensDeProgramacao);
+
+
+// 4 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaDeNomes = ["Rafael", "Karen", "Ayla"];
+console.log(listaDeNomes[0]);
+
+
+// 5 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaDeNomesDois = ["Rafael", "Karen", "Ayla"];
+console.log(listaDeNomesDois[1]);
+
+
+// 6 - Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaDeNomesTres = ["Rafael", "Karen", "Ayla"];
+console.log(listaDeNomesTres[listaDeNomesTres.length -1]);
+
+
+
+
+
